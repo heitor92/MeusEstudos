01c9d2c121f9336dadd5c46464df58d36015e05c
Heitor de Souza <heisou06@gmail.com>
2019-08-20 16:18:54 -0400

    avancando em php

---
+++ Avancando_php/get.php
@@ -0,0 +1,21 @@
+<?php
+
+//echo $_GET['planeta'];
+//echo '<br>';
+//echo $_GET['cor'];
+
+//?planeta=Marte&cor=azul = Query String
+
+$planeta = isset($_GET['planeta']) ? $_GET['planeta'] : false;
+
+if($planeta != false){
+    echo $planeta;
+} else {
+    echo "Planeta não informado!";
+}
+
+$cor = $_GET['cor'] ?? "Cor não informada!";
+
+//coalesce (??) do PHP 7
+//o PHP entende que eu vou testar essa variavel com esse índice se ele existe ou não.
+//No exemplo acima ele vai verificar se $_GET['cor'] foi definido se sim ele vai atribuir na variavel $cor.
