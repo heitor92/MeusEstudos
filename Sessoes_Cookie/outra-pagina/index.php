d316d5923c050865b2bd371a8da0154c70dba9d5
Heitor de Souza <heisou06@gmail.com>
2019-09-02 15:25:15 -0400

    Sessoes e Cookies

---
+++ Sessoes_Cookie/outra-pagina/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*Acessando o cookie criado */
+
+//var_dump($_COOKIE);
+
+/*
+    Acessando a sessão
+session_save_path — Obtém e/ou define o caminho para armazenamento da sessão atual.
+
+*/
+//var_dump(session_save_path());
+//session_start();
+//var_dump($_SESSION['usuario']);
+
